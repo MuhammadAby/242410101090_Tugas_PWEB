ed31ad2dc2bc54a3a005485887c861b754f1e876
MochammadAby <isfamaulanaabi1@gmail.com>
2026-06-17 09:36:32 +0700

    Hapus tag vite agar tidak error di server

--- PeduliKita_TM/resources/views/layouts/admin.blade.php
+++ PeduliKita_TM/resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin PeduliKita</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
     {{-- Font Awesome --}}
     <link rel="stylesheet"
